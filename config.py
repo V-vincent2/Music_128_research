637aece385f46765bd6b0ed7120827e09ec26304
V-vincent2 <108282742+V-vincent2@users.noreply.github.com>
2022-09-19 06:55:06 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+"""
+Created on Wed Jan 29 10:29:55 2020
+
+@author: vlad
+"""
+
+#insert local parameters
+
+username = '803ub6xz842fkf7dx7dn50x2g'
+client_id ='73140e3bda034374ab32aa01606661bd'
+client_secret = 'ea8ba1af68614fe799b9c37830fa2189'
+redirect_uri = 'http://localhost:7777/callback'
+scope = 'user-read-recently-played'
